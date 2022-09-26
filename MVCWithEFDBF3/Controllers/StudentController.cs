3414196c7c41fe735a81eda7fde0856932f79775
Yaswanth <pyaswanth4@gmail.com>
2022-09-26 12:02:33 +0530

    Add project files.

---
+++ MVCWithEFDBF3/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using MVCWithEFDBF3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithEFDBF3.Controllers
+{
+    public class StudentController : Controller
+    {
+      MVCDBEntities dc = new MVCDBEntities();
+        public ViewResult DisplayStudents()
+        {
+            return View(dc.Student_Select(null, true););
+        }
+    }
+}
